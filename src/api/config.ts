b5a1461fe69e21ca64d018796098c2491af7ae83
AkarshanGhosh <ronighosh494@gmail.com>
2025-06-14 04:15:46 +0530

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://backend-ai-vision.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
